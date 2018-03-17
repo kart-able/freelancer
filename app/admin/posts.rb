db825e0778d8cb66c0fd7f3bd753c4f2ec1ac05b
Jean-Claude <jeanclaude.wallard@gmail.com>
2018-03-17 06:49:26 +0100

    strong parameters for users and posts in admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -12,4 +12,27 @@ ActiveAdmin.register Post do
 #   permitted
 # end
 
+  permit_params :title, :body
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :title
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body
+    end
+    f.actions
+  end
+
 end
