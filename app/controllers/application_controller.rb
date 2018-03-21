7cfcff6e8aba9787b624618699a756a27b9e5feb
Jean-Claude <jeanclaude.wallard@gmail.com>
2018-03-21 18:39:14 +0100

    install of cancancan set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 #  before_action :authenticate_user!
 
+  # ajout cf https://activeadmin.info/13-authorization-adapter.html
+  # en lien avec active_admin.rb ligne 81
+  def access_denied(exception)
+    redirect_to admin_organizations_path, alert: exception.message
+  end
+
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -63,7 +63,7 @@ ActiveAdmin.setup do |config|
   # method in a before filter of all controller actions to
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
-  # config.authorization_adapter = ActiveAdmin::CanCanAdapter
+  config.authorization_adapter = ActiveAdmin::CanCanAdapter # cf http://github-docs.activeadmin.info/13-authorization-adapter.html
 
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
@@ -78,7 +78,8 @@ ActiveAdmin.setup do |config|
   # because, by default, user gets redirected to Dashboard. If user
   # doesn't have access to Dashboard, he'll end up in a redirect loop.
   # Method provided here should be defined in application_controller.rb.
-  # config.on_unauthorized_access = :access_denied
+  config.on_unauthorized_access = :access_denied # cf http://github-docs.activeadmin.info/13-authorization-adapter.html
+# voir ajout dans application_controller.rb
 
   # == Current User
   #
